70263ab178f0e78ef69b94dc842864f3f10b2e84
dj0nny <frank96990@gmail.com>
2019-09-01 11:11:34 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <template>
   <div id="app">
+    <Header />
     <router-view/>
   </div>
 </template>
+
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Verdana, Geneva, sans-serif;
+  font-size: 10pt;
+  color: #828282;
+  background-color: rgb(246, 246, 239);
+  width: 85%;
+  margin: auto;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="header">
+    <div class="logo">
+      <img src="https://news.ycombinator.com/y18.gif" />
+      <span class="title">Hacker news</span>
+    </div>
+    <div class="menu-container">
+      <ul id="menu">
+        <li class="item"><router-link to="/">news</router-link></li> |
+        <li class="item"><router-link to="/newest">newest</router-link></li> |
+        <li class="item"><router-link to="/ask">ask</router-link></li> |
+        <li class="item"><router-link to="/show">show</router-link></li> |
+        <li class="item"><router-link to="/jobs">jobs</router-link></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+
+<style scoped>
+.header {
+  background-color: rgb(255, 102, 0);
+  color: #222222;
+  padding: 3px;
+  display: flex;
+}
+
+.logo {
+  display: flex;
+}
+
+.logo img {
+  outline: 1px solid #fff;
+}
+
+.logo .title {
+  display: block;
+  margin-left: 5px;
+  font-weight: bold;
+}
+
+ul#menu {
+  margin: 0;
+  list-style-type: none;
+  padding-left: 15px;
+}
+
+ul#menu li.item {
+  display: inline-block;
+}
+
+ul#menu li.item a {
+  color: #222222;
+  padding-right: 3px;
+  padding-left: 3px;
+  text-decoration: none;
+}
+
+ul#menu li.item:last-child a {
+  margin-right: 0;
+}
+
+</style>
